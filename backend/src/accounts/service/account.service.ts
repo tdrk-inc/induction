522199c6c665e63a5f5511958b3826b6cec4dabc
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-03-22 04:05:54 +0900

    サインアップ関数をアカウントサービスに作成.

--- backend/src/accounts/service/account.service.ts
+++ backend/src/accounts/service/account.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Account } from "../domain/entities/account.entity";
 import { Repository } from "typeorm";
+import { CreateAccountInput } from "../interface/requests/create-account.input";
+import { createHash } from "crypto";
 
 @Injectable()
 export class AccountService {
@@ -9,4 +11,9 @@ export class AccountService {
     @InjectRepository(Account)
     private readonly repository: Repository<Account>
   ) {}
+
+  async signup({ password, ...input }: CreateAccountInput): Promise<Account> {
+    const hash = createHash("sha256").update(password);
+    return this.repository.save({ password: hash.digest("hex"), ...input });
+  }
 }
