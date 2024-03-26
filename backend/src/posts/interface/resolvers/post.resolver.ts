4f739a67bf2bb5412bc252bb29b79f73c7aa4392
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-03-26 22:04:48 +0900

    ポストスキーマを作成.

--- backend/src/posts/interface/resolvers/post.resolver.ts
+++ backend/src/posts/interface/resolvers/post.resolver.ts
@@ -6,17 +6,18 @@ import {
 import { PostService } from "src/posts/service/post.service";
 import { CreatePostInput } from "../requests/create-post.input";
 import { UseGuards } from "@nestjs/common";
+import { GraphQLPost } from "../responses/post.graphql";
 
 @UseGuards(AccountGuard)
 @Resolver()
 export class PostResolver {
   constructor(private readonly service: PostService) {}
 
-  @Mutation()
+  @Mutation(() => GraphQLPost)
   async createPost(
     @Context() context: AccountGuardContext,
     @Args({ name: "input", type: () => CreatePostInput }) input: CreatePostInput
-  ) {
+  ): Promise<GraphQLPost> {
     return this.service.create(context.accountId, input);
   }
 }
