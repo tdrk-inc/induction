b4607302f9caf7d04fe1bce7993016aa89729ecc
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-03-26 22:04:37 +0900

    ポストモジュールを作成.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,7 @@ import { join } from "path";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
 import { AccountModule } from "./accounts/account.module";
+import { PostModule } from "./posts/post.module";
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { AccountModule } from "./accounts/account.module";
     }),
     DatabaseModule,
     AccountModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppResolver, AppService],
