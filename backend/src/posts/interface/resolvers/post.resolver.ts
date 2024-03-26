81f01233a77e142fd49f932f4d02da2d47c1a256
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-03-27 03:06:35 +0900

    リゾルバに複数検索の関数を作成.

--- backend/src/posts/interface/resolvers/post.resolver.ts
+++ backend/src/posts/interface/resolvers/post.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Context, Int, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Context, Int, Mutation, Query, Resolver } from "@nestjs/graphql";
 import {
   AccountGuard,
   AccountGuardContext,
@@ -36,5 +36,11 @@ export class PostResolver {
     @Args({ name: "id", type: () => Int }) id: number
   ): Promise<GraphQLPost> {
     return new GraphQLPost(await this.service.remove(context.accountId, id));
+  }
+
+  @Query(() => [GraphQLPost], { name: "posts" })
+  async find(@Context() context: AccountGuardContext): Promise<GraphQLPost[]> {
+    const posts = await this.service.find(context.accountId);
+    return posts.map((v) => new GraphQLPost(v));
   }
 }
