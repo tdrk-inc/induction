3b8bf8a436e969243e47c496b411fe9a9d9ccf01
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-03-26 20:46:03 +0900

    AccountGuardを作成.

--- backend/src/accounts/account.module.ts
+++ backend/src/accounts/account.module.ts
@@ -4,9 +4,10 @@ import { AccountResolver } from "./interface/resolvers/account.resolver";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Account } from "./domain/entities/account.entity";
 import { ConfigModule } from "@nestjs/config";
+import { AccountGuard } from "./middleware/account.guard";
 
 @Module({
-  providers: [AccountService, AccountResolver],
+  providers: [AccountService, AccountResolver, AccountGuard],
   imports: [TypeOrmModule.forFeature([Account]), ConfigModule],
 })
 export class AccountModule {}
