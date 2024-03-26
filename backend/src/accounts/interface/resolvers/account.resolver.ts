f0e35be4d3e2e8eead6ef7a42daedc35d39b058b
shimizu1219 <atsuyoshi.shimizu@tdrk-inc.co.jp>
2024-03-26 19:44:11 +0900

    リゾルバにサインイン関数を作成.

--- backend/src/accounts/interface/resolvers/account.resolver.ts
+++ backend/src/accounts/interface/resolvers/account.resolver.ts
@@ -1,6 +1,7 @@
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { AccountService } from "src/accounts/service/account.service";
 import { SignupAccountInput } from "../requests/signup-account.input";
+import { SigninAccountInput } from "../requests/signin-account.input";
 
 @Resolver()
 export class AccountResolver {
@@ -13,5 +14,13 @@ export class AccountResolver {
   ): Promise<string> {
     const account = await this.service.signup(input);
     return account.name;
+  }
+
+  @Query(() => String)
+  async signin(
+    @Args({ name: "input", type: () => SigninAccountInput })
+    input: SigninAccountInput
+  ): Promise<string> {
+    return this.service.signin(input);
   }
 }
